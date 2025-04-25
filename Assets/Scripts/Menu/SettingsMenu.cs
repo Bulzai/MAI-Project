4a449797323fc08801728439489afd53e6c9db23
cruzifixu <franchezcacruz09@gmail.com>
2025-04-25 14:34:28 +0200

    Added some logic
    - Play
        -  switch to MovementScene
    - Options
        - toggle Fullscreen
        - set Audio Volume
        - back to Main Menu
    - Quit
        - close Application

--- Assets/Scripts/Menu/SettingsMenu.cs
+++ Assets/Scripts/Menu/SettingsMenu.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
-public class MainMenu : MonoBehaviour
+public class SettingsMenu : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,14 +19,13 @@ public class MainMenu : MonoBehaviour
         
     }
 
-    public void PlayGame()
+    public void SetVolume(float volume)
     {
-        SceneManager.LoadScene("MovementScene");
+        audioMixer.SetFloat("volume", volume);
     }
 
-    public void QuitGame()
+    public void SetFullscreen(bool isFullscreen)
     {
-        Debug.Log("Quit");
-        Application.Quit();
+        Screen.fullScreen = isFullscreen;
     }
 }
